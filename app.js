92eb52756cc641f5a9f6e203d49e3882e0c65123
Nayeem <nayeem.hasanewu@gmail.com>
2021-02-17 17:45:27 +0600

    ejs templete complete

--- app.js
+++ app.js
@@ -3,12 +3,10 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.engine('handlebars',expressHbs({layoutsDir: 'views/layouts', defaultLayout: 'main-layout', extname: 'handlebars'}));
-app.set('view engine', 'handlebars');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin'); 
